2564b0df0350d3e3d29b56c84fae667c3d9f7ff3
Faith Ye <faith.ye@hotmail.com>
2022-09-14 22:08:04 +0800

    enum required

--- models/Activity.js
+++ models/Activity.js
@@ -6,7 +6,11 @@ const activitySchema = new mongoose.Schema({
   activityStartDate: Date,
   activityEndDate: Date,
   personInCharge: String,
-  status: {type: String, enum: ["Pending", "Upcoming", "In Progress", "Completed", "Cancelled"]},
+  status: {
+    type: String,
+    enum: ["Pending", "Upcoming", "In Progress", "Completed", "Cancelled"],
+    required: true,
+  },
   photos: [String],
   // comments: {type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
 });
