92885735cf5eca5bb0ec9551ec954e1e5e363fd9
Kenzothd <Kenzothd@hotmail.com>
2022-09-16 11:20:59 +0800

    server kenzo updates

--- controller/ActivityController.js
+++ controller/ActivityController.js
@@ -12,115 +12,107 @@ router.get("/", async (req, res) => {
   }
 });
 
-
-
-
 // Seed Activities
-router.get("/seed", async (req,res)=> {
+router.get("/seed", async (req, res) => {
+  const activities = [
+    {
+      activityTitle: "Hacking of walls",
+      activityDescription:
+        "Hackers will reach around 10am to start hacking and clear tiles until 4pm. There will be lots of noise and dust.",
+      activityStartDate: new Date(),
+      activityEndDate: new Date(),
+      personInCharge: "Mr Ye",
+      status: "Upcoming",
+      photos: ["url", "url", "url"],
+    },
+    {
+      activityTitle: "Hacking of toilet",
+      activityDescription:
+        "Hackers will reach around 11am to start hacking and toilet.",
+      activityStartDate: new Date(),
+      activityEndDate: new Date(),
+      personInCharge: "Mr Ye",
+      status: "Pending",
+      photos: ["url", "url", "url"],
+    },
+    {
+      activityTitle: "Install toilet piping",
+      activityDescription: "Worker will reach around 11am to install piping.",
+      activityStartDate: new Date(),
+      activityEndDate: new Date(),
+      personInCharge: "Mr Ye",
+      status: "Completed",
+      photos: ["url", "url", "url"],
+    },
+  ];
 
-const activities = [
-	{
-		activityTitle: "Hacking of walls",
-		activityDescription: "Hackers will reach around 10am to start hacking and clear tiles until 4pm. There will be lots of noise and dust.",
-		activityStartDate: "2022-09-14T13:31:08.355Z",
-		activityEndDate: "2022-09-14T13:31:08.355Z",
-		personInCharge: "Mr Ye",
-		status: "Ongoing",
-
-	},
-	{
-
-		activityTitle: "Hacking of toilet",
-		activityDescription: "Hackers will reach around 11am to start hacking and toilet.",
-		activityStartDate: "2022-09-14T13:31:08.355Z",
-		activityEndDate: "2022-09-14T13:31:08.355Z",
-		personInCharge: "Mr Ye",
-		status: "Ongoing",
-	
-	},
-	{
-		activityTitle: "Install toilet piping",
-		activityDescription: "Worker will reach around 11am to install piping.",
-		activityStartDate: "2022-09-14T13:31:08.355Z",
-		activityEndDate: "2022-09-14T13:31:08.355Z",
-		personInCharge: "Mr Ye",
-		status: "Ongoing",
-
-	}
-]
-
-  await Activity.deleteMany()
-  try{
-    const seedActivities = await Activity.create(activities)
-    res.status(200).send(seedActivities);    
-  }catch (err) {
+  await Activity.deleteMany();
+  try {
+    const seedActivities = await Activity.create(activities);
+    res.status(200).send(seedActivities);
+  } catch (err) {
     res.status(500).send({ err });
   }
-
-})
-
-
-
+});
 
 //* Create Activity
-router.post('/', async (req,res)=> {
-  const activityData = req.body
+router.post("/", async (req, res) => {
+  const activityData = req.body;
   try {
-    const activity = await Activity.create(activityData)
+    const activity = await Activity.create(activityData);
     res.status(200).send(activity);
   } catch (err) {
     res.status(500).send({ err });
   }
-})
-
+});
 
 //Show 1 Activity
-router.get('/id/:id', async (req,res)=> {
-  const {id} = req.params
+router.get("/id/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const newActivity = await Activity.findById(id)
+    const newActivity = await Activity.findById(id);
     if (newActivity === null) {
-      res.status(400).send({error: "No activity found!"});
+      res.status(400).send({ error: "No activity found!" });
     }
     res.status(200).send(newActivity);
-
   } catch (err) {
     res.status(500).send({ err });
   }
-})
-
+});
 
 //Update Client
-router.put('/id/:id', async (req,res)=> {
-  const {id} = req.params
-  const activityUpdates = req.body
+router.put("/id/:id", async (req, res) => {
+  const { id } = req.params;
+  const activityUpdates = req.body;
   try {
-    const updatedActivity = await Activity.findByIdAndUpdate(id, activityUpdates, {new: true})
+    const updatedActivity = await Activity.findByIdAndUpdate(
+      id,
+      activityUpdates,
+      { new: true }
+    );
     if (updatedActivity === null) {
-      res.status(400).send({error: "No activity found!"});
+      res.status(400).send({ error: "No activity found!" });
     } else {
       res.status(200).send(updatedActivity);
     }
   } catch (err) {
     res.status(500).send({ err });
   }
-})
-
+});
 
 //Delete Client
-router.delete('/id/:id', async (req,res)=> {
-  const {id} = req.params
+router.delete("/id/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const deleteActivity = await Activity.findByIdAndDelete(id)
+    const deleteActivity = await Activity.findByIdAndDelete(id);
     if (deleteActivity === null) {
-      res.status(400).send({error: "No activity found!"});
+      res.status(400).send({ error: "No activity found!" });
     } else {
       res.status(200).send(deleteActivity);
     }
   } catch (err) {
     res.status(500).send({ err });
   }
-})
-
+});
 
 module.exports = router;
