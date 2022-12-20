afdd4e89f83040b7601dc596e3eddfaa207c1c6d
faithyeenx5 <faith.ye@hotmail.com>
2022-12-20 21:25:25 +0800

    adding stuff

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ mongoose.connect(MONGO_URI, {}, () => {
 
 //*middleware
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan("dev")); // it prints out any pings done in the back end
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
