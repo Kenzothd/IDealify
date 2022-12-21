15b158fcafe4abd157764145a7bf28e654a44ce9
Kenzothd <Kenzothd@hotmail.com>
2022-12-21 13:52:28 +0800

    kenzo update

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const PortfolioController = require("./controller/PortfolioController");
 //config
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-const MONGO_URI =
-  "mongodb+srv://sei38:sei38@cluster0.gndtvgd.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI;
 const { cloudinaryName, cloudinaryApiKey, cloudinaryApiSecret } = process.env;
 
 mongoose.connection.once("open", () => {
